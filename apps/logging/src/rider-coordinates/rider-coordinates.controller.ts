b795fad2cd71000b3754b097b3b14c1af70b1f6a
Deezi <amuhdavid@gmail.com>
2025-12-23 18:09:32 +0100

    update

--- apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
+++ apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCordinatesDTO } from './dto/create-cordinates.dto';
 
 @Controller('rider-coordinates')
 export class RiderCoordinatesController {
     @Get()
     getRiderCoordinates(){
         return "I'm from rider coordinates test"
+    }
+
+    @Post()
+    saveRiderCoordinates(
+        @Body()
+        createCordinate:CreateCordinatesDTO
+    ){
+        return createCordinate
     }
 }
